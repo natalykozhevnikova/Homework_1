87aafdb76715c58a7d361c19410a5d464441fbed
kozhevnikovanataly@yandex.ru <kozhevnikovanataly@yandex.ru>
2023-02-06 20:35:38 +0300

    commit

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HomeWork1 {
+
+import java.util.Scanner;
+public class HomeWork1 {
+    public static void name(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println('Введите число: ');
+        int a = sc.nextInt();
+        sc.close();
+        System.out.println("Треугольное число = " + Triangle(a) + "\n");
+    }
+    public static int Triangle(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return Triangle(num - 1) + num;
+    }
 }
+
